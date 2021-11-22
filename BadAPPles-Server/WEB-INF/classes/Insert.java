724c2366b0f345dfd1242e64503dc82c50d11ce6
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-11-21 23:56:10 -0800

    Update Insert.java

--- BadAPPles-Server/WEB-INF/classes/Insert.java
+++ BadAPPles-Server/WEB-INF/classes/Insert.java
@@ -14,25 +14,26 @@ public class Insert extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        
         // reader for user input
         InputStream reader = request.getInputStream();
+        
         // files
         String[] fileNames = {"Uber-Jan-Feb-FOIL", "other-Dial7_B00887", "other-Firstclass_B01536",
                               "uber-raw-data-apr14","other-Federal_02216", "other-Highclass_B01717"};
 
-         // parameters from client
+       // parameters from client
         String fileParam = request.getParameter("param1");
         String newRow = request.getParameter("param2");
         
-        // convert necessary parameters to ints
-        int fileNum = Integer.parseInt(fileParam);
+        int fileNum = Integer.parseInt(fileParam); // convert necessary parameters to ints
 
         String splitBy = ",";
         String line[] = newRow.split(splitBy);
 
         String file = fileNames[fileNum] + ".csv";
 
-        // create file and load into dataList
+       // create file and load into dataList
         File newFile = new File("webapps/BadAPPles/temp/" + file);
         InputStream inputStream = new FileInputStream(newFile);
         CSVFile csvFile = new CSVFile(inputStream);
@@ -41,15 +42,15 @@ public class Insert extends HttpServlet{
         
         data.insert(line);
 
-        // delete old file
+       // delete old file
         CleanDir clean = new CleanDir();
         clean.removeFile(new File("webapps/BadAPPles/temp/" + file));
 
         // write the dataList to the backup
         csvFile.write(data.getData(), "webapps/BadAPPles/temp/" + file);
     
-        // update cache if it exists
-        if (fileNum == 1) { // Dial
+       // update cache if it exists
+        if (fileNum == 1) {  // Dial
             File Top3Hours = new File("webapps/BadAPPles/cache/Top3Hours.csv");
             File Top3Streets = new File("webapps/BadAPPles/cache/Top3Streets.csv");
             File TopStreetsTime = new File("webapps/BadAPPles/cache/TopStreetsTime.csv");
@@ -64,7 +65,7 @@ public class Insert extends HttpServlet{
                 int[] columns = {0};
                 incrementCache(Top3Streets, "webapps/BadAPPles/cache/Top3Streets.csv", newValues, columns);
             }
-            if (TopStreetsTime.exists()){
+            if (TopStreetsTime.exists()) {
                 int newTime = Integer.parseInt(line[1].split(":")[0].trim());
                 int newInterval = 0;
                 if (newTime < 8) {
@@ -82,14 +83,14 @@ public class Insert extends HttpServlet{
                 incrementCache(TopStreetsTime, "webapps/BadAPPles/cache/TopStreetsTime.csv", newValues, columns);
             }
         }
-        else if (fileNum == 2 || fileNum == 5) { // FirstClass or HighClass
+        else if (fileNum == 2 || fileNum == 5) {  // FirstClass or HighClass
             File TopVehicleAM = new File("webapps/BadAPPles/cache/TopVehicleAM.csv");
             File TopVehiclePM = new File("webapps/BadAPPles/cache/TopVehiclePM.csv");
             String newTime = line[1].split(":")[0].trim();
 
             System.out.println("topVehicle here");
 
-            if (TopVehicleAM.exists() && line[1].contains("AM")) { // if file exists and time column was updated
+            if (TopVehicleAM.exists() && line[1].contains("AM")) {  // if file exists and time column was updated
                 String vehicleClass;
                 if (fileNum == 2) {
                     System.out.println("first class");
@@ -99,14 +100,14 @@ public class Insert extends HttpServlet{
                     System.out.println("high class");
                     vehicleClass = "HighClass";
                 }
-                if (line[1].contains("AM")){
+                if (line[1].contains("AM")) {
                     // increment old value
                     String[] values = {newTime, vehicleClass};
                     int[] columns = {0, 1};
                     incrementCache(TopVehicleAM, "webapps/BadAPPles/cache/TopVehicleAM.csv", values, columns);
                 }
             }
-            if (TopVehiclePM.exists() &&  line[1].contains("PM")) { // if file exists and time column was updated
+            if (TopVehiclePM.exists() &&  line[1].contains("PM")) {  // if file exists and time column was updated
                 String vehicleClass;
                 if (fileNum == 2) {
                     System.out.println("first class");
@@ -117,8 +118,8 @@ public class Insert extends HttpServlet{
                     vehicleClass = "HighClass";
                 }
 
-                if (line[1].contains("PM")){
-                    // increment old value
+                if (line[1].contains("PM")) {
+                   // increment old value
                     String[] values = {newTime, vehicleClass};
                     int[] columns = {0, 1};
                     incrementCache(TopVehiclePM, "webapps/BadAPPles/cache/TopVehiclePM.csv", values, columns);
@@ -126,7 +127,7 @@ public class Insert extends HttpServlet{
 
             }
         }
-        else if (fileNum == 4) { // Federal (index 6)
+        else if (fileNum == 4) {  // Federal (index 6)
             File NumPeopleArrived = new File("webapps/BadAPPles/cache/NumPeopleArrived.csv");
             if (NumPeopleArrived.exists()) {
                 String[] newValues = {line[line.length-1].trim()};
@@ -135,7 +136,7 @@ public class Insert extends HttpServlet{
              }
 
         }
-        else if (fileNum == 3) { // apr
+        else if (fileNum == 3) {  // apr
             File LatLong = new File("webapps/BadAPPles/cache/LatLong.csv");
             if (LatLong.exists()) {
                 String[] newValues = {line[1].trim() + line[2].trim()};
@@ -143,13 +144,16 @@ public class Insert extends HttpServlet{
                 incrementCache(LatLong, "webapps/BadAPPles/cache/LatLong.csv", newValues, columns);
             }
         }
+        
         reader.close();
         inputStream.close();
     }
 
+   ///////// helper functions
+    
     void incrementCache(File file, String fileName, String[] values, int[] columns) {
         try {
-        // create file and load into dataList
+           // create file and load into dataList
             InputStream inputStream = new FileInputStream(file);
             CSVFile csvFile = new CSVFile(inputStream);
             List<String[]> csvList = csvFile.read();
@@ -180,7 +184,7 @@ public class Insert extends HttpServlet{
                 }
 
             }
-            if (isNewValue){ // if new value isn't an existing value then add it to the cache
+            if (isNewValue){  // if new value isn't an existing value then add it to the cache
 
                 System.out.println("new cache value");
 
