27e5ce97963efefa8c3833125ea2abcdb0cd0a55
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-11-22 00:10:58 -0800

    Update Top3Hours.java

--- BadAPPles-Server/WEB-INF/classes/Top3Hours.java
+++ BadAPPles-Server/WEB-INF/classes/Top3Hours.java
@@ -23,30 +23,30 @@ public class Top3Hours extends HttpServlet {
 
         if (oldCache.exists()) {
 
-        // read file into dataList
+           // read file into dataList
             InputStream inputStream = new FileInputStream(oldCache);
             CSVFile csvFile = new CSVFile(inputStream);
             List<String[]> csvList = csvFile.read();
             DataList data = new DataList(csvList);
 
 
-        // create array to store distinct times and their counts
+           // create array to store distinct times and their counts
             ArrayList<String> distinctTimes = new ArrayList<String>();
             ArrayList<Integer> distinctTimesCnt = new ArrayList<Integer>();
             
 
-        // get distinct times and their counts
-            for(int i = 1; i < data.getData().size(); i++){
+           // get distinct times and their counts
+            for (int i = 1; i < data.getData().size(); i++) {
                 String tempTime = data.getData().get(i)[0].trim();
                 int tempCnt = Integer.parseInt(data.getData().get(i)[1].trim());
                 distinctTimes.add(tempTime);
                 distinctTimesCnt.add(tempCnt);
             }
 
-        // create result (top3)
+           // create result (top3)
             String[] top3 = new String[3];
             int top3Count = 0;
-            for(int i = 0; i < 3; i++) {
+            for (int i = 0; i < 3; i++) {
                 int max = Collections.max(distinctTimesCnt);
                 int index = distinctTimesCnt.indexOf(max);
                 String time = distinctTimes.get(index);
@@ -55,19 +55,19 @@ public class Top3Hours extends HttpServlet {
                 distinctTimes.remove(index);
             }
 
-        // parse array into JSON string
+           // parse array into JSON string
             JSONstring parser = new JSONstring();
             String result = parser.toJSON(top3);
 
             inputStream.close();
 
-        // send results to client
+            // send results to client
             out.println(result);
 
         }
         else {
 
-        // read file into dataList
+           // read file into dataList
             File file = new File("webapps/BadAPPles/temp/other-Dial7_B00887.csv");
             InputStream inputStream = new FileInputStream(file);
             CSVFile csvFile = new CSVFile(inputStream);
@@ -75,13 +75,13 @@ public class Top3Hours extends HttpServlet {
             DataList data = new DataList(csvList);
 
 
-        // create array to store distinct times and their counts
+           // create array to store distinct times and their counts
             ArrayList<String> distinctTimes = new ArrayList<String>();
             ArrayList<Integer> distinctTimesCnt = new ArrayList<Integer>();
             
 
-        // get distinct times and their counts
-            for(int i = 1; i < data.getData().size(); i++){
+           // get distinct times and their counts
+            for (int i = 1; i < data.getData().size(); i++) {
                 String tempTime = data.getData().get(i)[1].split(":")[0].trim();
                 if ( !distinctTimes.contains(tempTime) ) {
                     distinctTimes.add(tempTime);
@@ -94,13 +94,13 @@ public class Top3Hours extends HttpServlet {
                 }
             }
 
-        // create result (top3) & cache data structures
+           // create result (top3) & cache data structures
             List<String[]> cachedResults = new ArrayList<String[]>();
             String[] columns = {"Hour", "Count"};
             cachedResults.add(columns);
             String[] top3 = new String[3];
             int top3Count = 0;
-            for(int i = 0; i < distinctTimes.size() ; i++) {
+            for (int i = 0; i < distinctTimes.size() ; i++) {
                 int max = Collections.max(distinctTimesCnt);
                 int index = distinctTimesCnt.indexOf(max);
                 String time = distinctTimes.get(index);
@@ -113,28 +113,28 @@ public class Top3Hours extends HttpServlet {
                 distinctTimes.remove(index);
             }
 
-        // parse array into JSON string
+           // parse array into JSON string
             JSONstring parser = new JSONstring();
             String result = parser.toJSON(top3);
 
             inputStream.close();
 
-        // send results to client
+           // send results to client
             out.println(result);
 
 
-        // create DataList object from cachedResults
+           // create DataList object from cachedResults
             DataList dataCache = new DataList(cachedResults);
 
-        // create cache file
+           // create cache file
             File fileCache = new File("webapps/BadAPPles/cache/Top3Hours.csv");
             fileCache.createNewFile();
 
-        // write the DataList to the cache file
+           // write the DataList to the cache file
             csvFile.write(dataCache.getData(), "webapps/BadAPPles/cache/Top3Hours.csv");
 
         }
-
+        
     }
 }
 
