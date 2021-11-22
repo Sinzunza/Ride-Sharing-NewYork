1933364f0cd2dacc77c0e3f22b045a735b56359b
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-11-22 00:07:43 -0800

    Update NumPeopleArrived.java

--- BadAPPles-Server/WEB-INF/classes/NumPeopleArrived.java
+++ BadAPPles-Server/WEB-INF/classes/NumPeopleArrived.java
@@ -1,4 +1,5 @@
 import java.io.*;
+
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.*;
@@ -20,41 +21,39 @@ public class NumPeopleArrived extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
 
-        Map<String, Integer> map = new HashMap<String, Integer>(); // keys: Arrived, Cancelled, Assigned
+        Map<String, Integer> map = new HashMap<String, Integer>();  // keys: Arrived, Cancelled, Assigned
 
-        ///*
         File tempFile = new File("webapps/BadAPPles/cache/NumPeopleArrived.csv");
         boolean exists = tempFile.exists();
-        if (exists){
-            //get results from cache
+        if (exists) {
+           // get results from cache
             BufferedReader br = new BufferedReader(new FileReader("webapps/BadAPPles/cache/NumPeopleArrived.csv"));
             String line = null;
             while ((line = br.readLine()) != null) {
-                // read into map
+               // read into map
                 String[] col = line.split(",");
                 String status = col[0].trim();
                 String count = col[1].trim();
                 boolean notFirstLine = !col[1].equals("Count");
-                if(notFirstLine) {
+                if (notFirstLine) {
                     Integer cnt = Integer.parseInt(count);
                     map.put(status, cnt);
                 }
             }
             br.close();
 
-            //get values
+           // get values
             String[] stringArray = new String[3];
             stringArray[0] = "Arrived: " + String.valueOf(map.get("Arrived"));
             stringArray[1] = "Cancelled: " + String.valueOf(map.get("Cancelled"));
             stringArray[2] = "Assigned: " + String.valueOf(map.get("Assigned"));
 
-            // send results to client
+           // send results to client
             JSONstring parser = new JSONstring();
             String result = parser.toJSON(stringArray);
             out.println(result);
         }
         else { // do the stuff from previous sprints and at the end create a cache file that stores the counts for all hours not just the top 3
-            //*/
 
             BufferedReader br = new BufferedReader(new FileReader("webapps/BadAPPles/temp/other-Federal_02216.csv"));
             String line = null;
@@ -100,19 +99,17 @@ public class NumPeopleArrived extends HttpServlet {
             String result = parser.toJSON(stringArray);
             out.println(result); // send results to client
 
-            //SPRINT6 - create cache file
-            ///*
             File myObj = new File("webapps/BadAPPles/cache/NumPeopleArrived.csv");
-            if (myObj.createNewFile()) { // File created
+            if (myObj.createNewFile()) {  // File created
                 FileWriter myWriter = new FileWriter("webapps/BadAPPles/cache/NumPeopleArrived.csv");
                 myWriter.write("Status,Count\n");
-                for (Map.Entry<String, Integer> entry : map.entrySet()){
+                for (Map.Entry<String, Integer> entry : map.entrySet()) {
                     myWriter.write(entry.getKey() + "," + entry.getValue() + "\n");
                 }
                 myWriter.close();
             }
         }
-        //*/
+        
     }
 }
 
