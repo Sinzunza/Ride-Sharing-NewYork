7b0fde90f13c372d3884b48dcf99fd5a3417ab27
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-11-22 00:15:37 -0800

    Update TopStreetsTime.java

--- BadAPPles-Server/WEB-INF/classes/TopStreetsTime.java
+++ BadAPPles-Server/WEB-INF/classes/TopStreetsTime.java
@@ -1,4 +1,5 @@
 import java.io.*;
+
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.*;
@@ -9,13 +10,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
-
-//0-8, 9-16,17-24
-//street, count
-
 //How popularity of streets change through out the day
-
 
 public class TopStreetsTime extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -27,6 +22,7 @@ public class TopStreetsTime extends HttpServlet {
     private static final String col_separator = " , ";
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
         PrintWriter out = response.getWriter();
 
         Map.Entry<String, Integer> maxEntry = null;
@@ -35,11 +31,10 @@ public class TopStreetsTime extends HttpServlet {
         Map<String, Integer> interval2 = new HashMap<String, Integer>(); //0-6
         Map<String, Integer> interval3 = new HashMap<String, Integer>(); //0-6
 
-        ///*
         File tempFile = new File("webapps/BadAPPles/cache/TopStreetsTime.csv");
         boolean exists = tempFile.exists();
-        if (exists){
-            //get results from cache
+        if (exists) {
+           //get results from cache
             BufferedReader br = new BufferedReader(new FileReader("webapps/BadAPPles/cache/TopStreetsTime.csv"));
             String line = null;
             while ((line = br.readLine()) != null) {
@@ -48,23 +43,20 @@ public class TopStreetsTime extends HttpServlet {
                 String street = col[1].trim();
                 String count = col[2].trim();
                 boolean notFirstLine = !col[2].equals("Count");
-                if(notFirstLine) {
+                if (notFirstLine) {
                     Integer cnt = Integer.parseInt(count);
-                    if(col[0].trim().equals("1")) {
+                    if (col[0].trim().equals("1")) {
                         interval1.put(street, cnt);
                     }
-                    else if(col[0].trim().equals("2")){
+                    else if(col[0].trim().equals("2")) {
                         interval2.put(street, cnt);
                     }
-                    else if(col[0].trim().equals("3")){
+                    else if (col[0].trim().equals("3")) {
                         interval3.put(street, cnt);
                     }
                 }
             }
             br.close();
-
-            //get sorted values
-            //List<String> key1 = interval.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(3).map(Map.Entry::getKey).collect(Collectors.toList());
 
             List<String> key1 = interval1.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(1).map(Map.Entry::getKey).collect(Collectors.toList());
             List<String> key2 = interval2.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(1).map(Map.Entry::getKey).collect(Collectors.toList());
@@ -74,11 +66,8 @@ public class TopStreetsTime extends HttpServlet {
             int maxValueInMap3=(Collections.max(interval3.values()));
 
 
-            // send results to client
+           // send results to client
             String[] stringArray = new String[3];
-            //stringArray[0] = key1.get(0) + ": " + interval.get(key1.get(0));;
-            //stringArray[1] = key1.get(1) + ": " + interval.get(key1.get(1));;
-            //stringArray[2] = key1.get(2) + ": " + interval.get(key1.get(2));;
             stringArray[0] = key1.get(0) + ": " + String.valueOf(maxValueInMap1);;
             stringArray[1] = key2.get(0) + ": " + String.valueOf(maxValueInMap2);;
             stringArray[2] = key3.get(0) + ": " + String.valueOf(maxValueInMap3);;
@@ -86,9 +75,7 @@ public class TopStreetsTime extends HttpServlet {
             String result = parser.toJSON(stringArray);
             out.println(result);
         }
-        else
-        { // do the stuff from previous sprints and at the end create a cache file that stores the counts for all hours not just the top 3
-        //*/
+        else { // do the stuff from previous sprints and at the end create a cache file that stores the counts for all hours not just the top 3
 
             BufferedReader br = new BufferedReader(new FileReader("webapps/BadAPPles/temp/other-Dial7_B00887.csv"));
             String line = null;
@@ -103,30 +90,35 @@ public class TopStreetsTime extends HttpServlet {
 
                 try {
                     int x = (int) nf.parse(hour).doubleValue();
-                    //int x = Integer.parseInt(hour);
 
                     if (street.trim().isEmpty()) {
-                    } else if (x < 8) {
+                    }
+                    else if (x < 8) {
                         if (interval1.containsKey(street)) {
                             // just increment count
                             interval1.replace(street, interval1.get(street) + 1);
-                        } else {
+                        } 
+                        else {
                             // add it and increment count
                             interval1.put(street, 1);
                         }
-                    } else if (x < 16) {
+                    } 
+                    else if (x < 16) {
                         if (interval2.containsKey(street)) {
                             // just increment count
                             interval2.replace(street, interval2.get(street) + 1);
-                        } else {
+                        } 
+                        else {
                             // add it and increment count
                             interval2.put(street, 1);
                         }
-                    } else if (x < 24) {
+                    } 
+                    else if (x < 24) {
                         if (interval3.containsKey(street)) {
                             // just increment count
                             interval3.replace(street, interval3.get(street) + 1);
-                        } else {
+                        }
+                        else {
                             // add it and increment count
                             interval3.put(street, 1);
                         }
@@ -159,15 +151,10 @@ public class TopStreetsTime extends HttpServlet {
             String result = parser.toJSON(stringArray);
             out.println(result); // send results to client
 
-            //SPRINT6 - create cache file
-            ///*
             File myObj = new File("webapps/BadAPPles/cache/TopStreetsTime.csv");
             if (myObj.createNewFile()) { // File created
                 FileWriter myWriter = new FileWriter("webapps/BadAPPles/Cache/TopStreetsTime.csv");
                 myWriter.write("Interval,Street,Count\n");
-                //for (Map.Entry<String, Integer> entry : interval.entrySet()) {
-                //    myWriter.write("1" + entry.getKey() + ", " + entry.getValue() + "\n");
-                //}
                 for (Map.Entry<String, Integer> entry : interval1.entrySet()) {
                     myWriter.write("1," + entry.getKey() + ", " + entry.getValue() + "\n");
                 }
@@ -180,6 +167,6 @@ public class TopStreetsTime extends HttpServlet {
                 myWriter.close();
             }
         }
-        //*/
+        
     }
 }
