4230da4e6f207f4733855933e75df3123f300e83
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-11-21 23:33:19 -0800

    Update HowManyPast10Servlet.java

--- BadAPPles-Server/WEB-INF/classes/HowManyPast10Servlet.java
+++ BadAPPles-Server/WEB-INF/classes/HowManyPast10Servlet.java
@@ -13,29 +13,26 @@ public class HowManyPast10Servlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        //parsing a CSV file into Scanner class constructor
+       // parsing a CSV file into Scanner class constructor
         String line = "";
         String splitBy = ",";
-        try
-        {
-            //parsing a CSV file into BufferedReader class constructor
+        try {
+           // parsing a CSV file into BufferedReader class constructor
             BufferedReader br = new BufferedReader(new FileReader("webapps/BadAPPles/temp/uber-raw-data-apr14.csv"));
             int total = 0;
-            while ((line = br.readLine()) != null)   //returns a Boolean value
-            {
-                String[] column = line.split(splitBy);    // use comma as separator
+            while ((line = br.readLine()) != null) {  //returns a Boolean value
+                String[] column = line.split(splitBy);  // use comma as separator
                 PrintWriter out = response.getWriter();
-                if((column[0].contains(" 22:")) || column[0].contains(" 23:")){ // 10:00pm to 11:59pm
+                if ((column[0].contains(" 22:")) || column[0].contains(" 23:")) {  // 10:00pm to 11:59pm
                     total += 1;
                 }
-                if((line = br.readLine()) == null){
+                if ((line = br.readLine()) == null) {
                     out.println(total);
                 }
             }
 
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
