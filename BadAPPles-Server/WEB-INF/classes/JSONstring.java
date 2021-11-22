00c6428ae442f9567f30a74a9befbce17335fc8e
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-11-21 23:57:00 -0800

    Update JSONstring.java

--- BadAPPles-Server/WEB-INF/classes/JSONstring.java
+++ BadAPPles-Server/WEB-INF/classes/JSONstring.java
@@ -1,6 +1,5 @@
 public class JSONstring {
-
-    // might have to remove static to be compatible with app code
+    
     public String toJSON(String[] stringArray){
         String input = "";
         String splitBy = ": ";
@@ -8,7 +7,7 @@ public class JSONstring {
         String json = "{ \"" + columnName + "\" : [ ";
         for (int i = 0; i < stringArray.length; i++) {
             input = stringArray[i];
-            String[] column = input.split(splitBy);    // use : as separator
+            String[] column = input.split(splitBy);  // use : as separator
             json += "{ \"" + column[0] + "\" : \"" + column[1] + "\" }";
             if (i+1 != stringArray.length) {
                 json += ", ";
@@ -17,7 +16,7 @@ public class JSONstring {
                  return json += " ]}";
             }
         }
-        //return "Error with toJSON";
+        
         return json += " ]}"; // this is still valid JSON     
     }
 
