5183699f5e7513ab84a5114c27581397acac6470
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-11-21 23:18:11 -0800

    Update CreateTempServlet.java

--- BadAPPles-Server/WEB-INF/classes/CreateTempServlet.java
+++ BadAPPles-Server/WEB-INF/classes/CreateTempServlet.java
@@ -38,45 +38,38 @@ public class CreateTempServlet extends HttpServlet{
         File oldFile;
         
         //store backUp file into temp folder
-        switch (fileNum){
+        switch (fileNum) {
             case 0:
                 file = fileNames[fileNum] + ".csv";
                 clean.removeFile(new File("webapps/BadAPPles/temp/" + file));
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-0/" + backUpFile),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-0/" + backUpFile), Paths.get("webapps/BadAPPles/temp/" + file));
                 break;
             case 1:
                 file = fileNames[fileNum] + ".csv";
                 clean.removeFile(new File("webapps/BadAPPles/temp/" + file));
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-1/" + backUpFile),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-1/" + backUpFile), Paths.get("webapps/BadAPPles/temp/" + file));
                 break;
             case 2:
                 file = fileNames[fileNum] + ".csv";
                 clean.removeFile(new File("webapps/BadAPPles/temp/" + file));
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-2/" + backUpFile),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-2/" + backUpFile), Paths.get("webapps/BadAPPles/temp/" + file));
                 break;
             case 3:
                 file = fileNames[fileNum] + ".csv";
                 clean.removeFile(new File("webapps/BadAPPles/temp/" + file));
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-3/" + backUpFile),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-3/" + backUpFile), Paths.get("webapps/BadAPPles/temp/" + file));
                 break;
             case 4:
                 file = fileNames[fileNum] + ".csv";
                 clean.removeFile(new File("webapps/BadAPPles/temp/" + file));
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-4/" + backUpFile),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-4/" + backUpFile), Paths.get("webapps/BadAPPles/temp/" + file));
                 break;
             case 5:
                 file = fileNames[fileNum] + ".csv";
                 clean.removeFile(new File("webapps/BadAPPles/temp/" + file));
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-5/" + backUpFile),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-5/" + backUpFile), Paths.get("webapps/BadAPPles/temp/" + file));
                 break;
             case 100:
-
                 // clean cache of any old files
                 File dirCache = new File("webapps/BadAPPles/cache");
                 clean.cleanDirectory(dirCache);
@@ -87,33 +80,28 @@ public class CreateTempServlet extends HttpServlet{
 
                 // move all original files into temp folder
                 file = fileNames[0] + ".csv";
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-0/" + file),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-0/" + file), Paths.get("webapps/BadAPPles/temp/" + file));
 
                 file = fileNames[1] + ".csv";
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-1/" + file),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-1/" + file), Paths.get("webapps/BadAPPles/temp/" + file));
 
                 file = fileNames[2] + ".csv";
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-2/" + file),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-2/" + file), Paths.get("webapps/BadAPPles/temp/" + file));
 
                 file = fileNames[3] + ".csv";
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-3/" + file),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-3/" + file), Paths.get("webapps/BadAPPles/temp/" + file));
 
                 file = fileNames[4] + ".csv";
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-4/" + file),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-4/" + file), Paths.get("webapps/BadAPPles/temp/" + file));
 
                 file = fileNames[5] + ".csv";
-                Files.copy(Paths.get("webapps/BadAPPles/backUp-5/" + file),
-                    Paths.get("webapps/BadAPPles/temp/" + file));
+                Files.copy(Paths.get("webapps/BadAPPles/backUp-5/" + file), Paths.get("webapps/BadAPPles/temp/" + file));
 
                 break;
         }
 
     }
+    
 }
 
 //  javac -Xlint DataList.java CSVFile.java CleanDir.java -cp ../../../../lib/servlet-api.jar *.java
